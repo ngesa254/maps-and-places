fbc7228b1684623b3f75721648db69b5860fb4c6
ngesa marvin <marvinngesa@gmail.com>
2018-02-11 11:16:16 +0300

    add onclick listener to placesautocomplete

--- app/src/main/java/io/droidmarvin/mapsandplaces/MapsActivity.java
+++ app/src/main/java/io/droidmarvin/mapsandplaces/MapsActivity.java
@@ -3,7 +3,10 @@ package io.droidmarvin.mapsandplaces;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -28,6 +31,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         placeAutocompleteFragment = (PlaceAutocompleteFragment)
                 getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        placeAutocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+
+            }
+
+            @Override
+            public void onError(Status status) {
+
+            }
+        });
     }
 
 
